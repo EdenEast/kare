5bbd75f5f46bbe917d2f5d75cd33d883b3bdc89a
James Simpson <edenofest@gmail.com>
2023-08-13 00:31:14 -0400

    iterating on ui and understanding egui lib

---
+++ src/cli.rs
@@ -0,0 +1,80 @@
+use std::path::PathBuf;
+
+use clap::{crate_description, crate_version, Args, Parser, Subcommand};
+
+pub trait Run {
+    fn run(self) -> eyre::Result<()>;
+}
+
+#[derive(Debug, Parser)]
+#[command(
+    about = crate_description!(),
+    version = crate_version!(),
+)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Option<Cmd>,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Cmd {
+    Play(Play),
+    Record(Record),
+}
+
+/// Record mouse and keyboard events and save them into a file
+#[derive(Debug, Args)]
+#[command(
+    visible_alias("r"),
+    disable_colored_help(true),
+    disable_version_flag(true)
+)]
+pub struct Record {
+    /// Key to be used to stop recording
+    ///
+    /// The stop key can be any combination of keys that have to be either pressed or held down at
+    /// the same time. The key combination is a comma seperated list of keys.
+    ///
+    /// If no value is passed this is defaulted to `Escape` as the stop key.
+    ///
+    /// Example:
+    ///     Stop the recording with the key combo `Ctrl` + `F9` would be `ctrl,f9`.
+    #[arg(short, long)]
+    pub stop_key: Option<String>,
+
+    /// Output recorded events into path.
+    #[arg(value_name = "PATH")]
+    pub output: PathBuf,
+}
+
+/// Play recorded file
+#[derive(Debug, Args)]
+#[command(
+    visible_alias("p"),
+    disable_colored_help(true),
+    disable_version_flag(true)
+)]
+pub struct Play {
+    /// Number of iterations to be executed
+    #[arg(short, long)]
+    pub iterations: Option<u32>,
+
+    /// Key to be used to stop playback
+    ///
+    /// The stop key can be any combination of keys that have to be either pressed or held down at
+    /// the same time. The key combination is a comma seperated list of keys.  If no value is
+    /// passed this is defaulted to `Escape` as the stop key.
+    ///
+    /// Example:
+    ///     Stop the playback with the key combo `Ctrl` + `F9` would be `ctrl,f9`.
+    #[arg(short, long)]
+    pub stop_key: Option<String>,
+
+    /// Delay between two iterations
+    #[arg(short, long)]
+    pub delay: Option<u64>,
+
+    /// Input file to be played
+    #[arg(value_name = "PATH")]
+    pub output: PathBuf,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![allow(unused)]
+use clap::Parser;
+
+use crate::cli::Cli;
+
+pub mod cli;
+mod gui;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match cli.command {
+        Some(command) => match command {
+            cli::Cmd::Play(_) => todo!(),
+            cli::Cmd::Record(_) => todo!(),
+        },
+        None => gui::run().unwrap(),
+    }
 }
